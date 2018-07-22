ad415a772261e7690fd34c9794df9ddebf9cfa32
ClaudineiRAlves <claudineirangel@hotmail.com>
2018-07-22 19:44:51 -0300

    page admin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\page;
+use \Hcode\pageAdmin;
 
 $app  = new Slim();
 
@@ -12,6 +13,14 @@ $app->config('debug', true);
 $app->get('/', function() {
     
     $page = new page();
+
+    $page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+    
+    $page = new pageAdmin();
 
     $page->setTpl("index");
 
